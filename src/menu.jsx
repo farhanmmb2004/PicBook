8b44390c46e97f700349eb8e6c719ca5b7de4b32
farhan <farhankhanmmb2004@gmail.com>
2024-06-18 22:18:30 +0530

    adding navigation and content div

---
+++ src/menu.jsx
@@ -0,0 +1,10 @@
+export default function Menu(){
+return (
+    <menu>
+    <div className="option">Your profile</div>
+    <div className="option">bookmarks</div>
+    <div className="option">settings</div>
+    <div className="option">about us</div>
+    </menu>
+);
+}
