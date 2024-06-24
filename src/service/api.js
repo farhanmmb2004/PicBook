ac921a5f816cd449365c978b56a1828808a56437
farhan <farhankhanmmb2004@gmail.com>
2024-06-25 00:41:11 +0530

    adding random image feature

--- src/service/api.js
+++ src/service/api.js
@@ -3,7 +3,7 @@ let api_KEY="44401170-af74c4d624fed378eb4dca7a6";
 let base_URL=`https://pixabay.com/api/?key=${api_KEY}`;
 export let fetchImages=async()=>{
 try{
-    let response=await axios.get(`${base_URL}`);
+    let response=await axios.get(`${base_URL}&q=random&page=${Math.floor(Math.random()*100)}`);
     // console.log(response.data.hits);
     return response.data;
 }
